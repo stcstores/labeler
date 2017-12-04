20daf3faefc8bdc37cd10bd1bda0c4c2dc32b687
Luke Shiner <luke@lukeshiner.com>
2017-12-04 15:02:52 +0000

    Add label format.

--- labeler/label_formats.py
+++ labeler/label_formats.py
@@ -1,4 +1,6 @@
-from . label_format import LabelFormat
+from reportlab.graphics.barcode import eanbc
+
+from .label_format import LabelFormat
 
 
 class DefaultLabelFormat(LabelFormat):
@@ -12,3 +14,17 @@ class DefaultLabelFormat(LabelFormat):
 
     def get_horizontal_location(self):
         return self.width / 2
+
+
+class BarcodeLabelFormat(DefaultLabelFormat):
+
+    def make_label(self, label, lines):
+        x_location = 15
+        y_location = 20
+        widget_class = eanbc.Ean13BarcodeWidget
+        barcode = lines[0]
+        bar_height = self.height * 0.7
+        barcode_drawing = widget_class(
+            barcode, x=x_location, y=y_location,
+            barHeight=bar_height)
+        label.add(barcode_drawing)
