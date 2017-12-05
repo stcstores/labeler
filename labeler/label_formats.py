70f8f4123fe2ea5552fb137febc1a550f6d56cb9
Luke Shiner <luke@lukeshiner.com>
2017-12-05 12:15:45 +0000

    Add text to barcode label.

--- labeler/label_formats.py
+++ labeler/label_formats.py
@@ -1,3 +1,4 @@
+from reportlab.graphics import shapes
 from reportlab.graphics.barcode import eanbc
 
 from .label_format import LabelFormat
@@ -15,16 +16,29 @@ class DefaultLabelFormat(LabelFormat):
     def get_horizontal_location(self):
         return self.width / 2
 
+    @staticmethod
+    def wrap(text, line_length):
+        if line_length < line_length:
+            return [text]
+        split_index = text[:line_length].rfind(' ')
+        return [text[:split_index], text[split_index:]]
+
 
 class BarcodeLabelFormat(DefaultLabelFormat):
 
     def make_label(self, label, lines):
+        text = lines[1]
+        vertical_location = 15
+        for line in self.wrap(text, 30):
+            label.add(shapes.String(
+                self.get_horizontal_location(), vertical_location, line,
+                fontSize=8, fontName=self.font, textAnchor=self.text_anchor))
+            vertical_location -= 10
         x_location = 15
-        y_location = 20
+        y_location = 25
         widget_class = eanbc.Ean13BarcodeWidget
         barcode = lines[0]
-        bar_height = self.height * 0.7
+        bar_height = self.height * 0.5
         barcode_drawing = widget_class(
-            barcode, x=x_location, y=y_location,
-            barHeight=bar_height)
+            barcode, x=x_location, y=y_location, barHeight=bar_height)
         label.add(barcode_drawing)
