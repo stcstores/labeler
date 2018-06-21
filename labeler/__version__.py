f514a58d57bfef717afa6511321ad92538b182d7
Luke Shiner <luke@lukeshiner.com>
2018-06-21 14:56:45 +0100

    Use __version__.py file.

---
+++ labeler/__version__.py
@@ -0,0 +1,10 @@
+"""Labeler."""
+
+__title__ = 'labeler'
+__description__ = 'Generate labels as printable PDFs.'
+__url__ = 'http://github.com/axevalley/labeler.git'
+__version__ = '0.1'
+__author__ = 'Luke Shiner'
+__author_email__ = 'luke@lukeshiner.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 Luke Shiner'
--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
+"""Setup for labeler."""
 
-from setuptools import setup, find_packages
+import os
 
-setup(
-    name='labeler',
-    version='1.0',
-    description='Creates printable labels',
-    author='Luke Shiner',
+import setuptools
+
+NAME = 'labeler'
+
+with open("README.rst", "r") as readme:
+    long_description = readme.read()
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, NAME, '__version__.py'), 'r') as f:
+    exec(f.read(), about)
+
+setuptools.setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=long_description,
+    url=about['__url__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
     install_requires=['reportlab', 'pylabels'],
-    packages=find_packages(),
-    )
+    packages=setuptools.find_packages(),
+)
