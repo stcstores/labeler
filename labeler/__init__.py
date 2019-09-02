31f41daed2d5702a281e730b38407648c77b6ff5
Luke Shiner <luke@lukeshiner.com>
2019-09-02 11:55:02 +0100

    chore: Add tools to ensure code consistency

--- labeler/__init__.py
+++ labeler/__init__.py
@@ -5,11 +5,10 @@ Generate PDF files for printing tiled labels.
 """
 
 from .label_format import LabelFormat  # NOQA
-from .label_size import LabelSize  # NOQA
-from .paper_size import PaperSize  # NOQA
-from .label_sheet import LabelSheet  # NOQA
-
 from .label_formats import *  # NOQA
-from .paper_sizes import *  # NOQA
-from .label_sizes import *  # NOQA
+from .label_sheet import LabelSheet  # NOQA
 from .label_sheets import *  # NOQA
+from .label_size import LabelSize  # NOQA
+from .label_sizes import *  # NOQA
+from .paper_size import PaperSize  # NOQA
+from .paper_sizes import *  # NOQA
